4a993f131df65c05d4a42acf7ebe23ccde5b6223
Your Name <1121853541@qq.com>
2021-03-31 14:46:21 +0800

    导入vant-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './styles/base.less'
 import './styles/iconfont.css'
+// 导入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
