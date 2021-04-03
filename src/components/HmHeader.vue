fdd8f5159d14bbd9e21ed541f8c5bae621cf508d
Your Name <1121853541@qq.com>
2021-04-03 11:43:55 +0800

    配置rem

---
+++ src/components/HmHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hm-header">
+    <div class="back">
+      <span class='iconfont iconjiantou2'></span>
+    </div>
+    <div class="title">
+      <slot></slot>
+    </div>
+    <div class="right">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,9 +3,18 @@
     我是登陆页面
     <span class="iconfont iconweixin"></span>
     <span class="iconfont icondianzan"></span>
+    <div class="box">
+    </div>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+<style lang='less'>
+.box{
+  width: 200px;
+  height: 300px;
+  background-color: aqua;
+}
+</style>
